f5f980aee15e3d121daa9f99b364449626744518
Chyrain <chy19910810@163.com>
2018-01-11 15:22:58 +0800

    use process.env to disable in production build

--- src/WithErrorHandler.js
+++ src/WithErrorHandler.js
@@ -1,102 +1,55 @@
-import React from 'react'
-import FallbackView from './Fallback'
+import React from "react";
+import FallbackView from "./Fallback";
 
 class ErrorBoundary extends React.PureComponent {
-    constructor () {
-        super();
+  constructor() {
+    super();
 
-        this.closeErrorModal = this.closeErrorModal.bind(this);
-        this.state = {
-            hasError: false,
-            error: null,
-            errorInfo: null
-        };
+    this.closeErrorModal = this.closeErrorModal.bind(this);
+    this.state = {
+      hasError: false,
+      error: null,
+      errorInfo: null
+    };
+  }
+
+  closeErrorModal() {
+    this.setState({ hasError: false });
+  }
+
+  componentDidCatch(error, info) {
+    // Update state if error happens
+    this.setState({ hasError: true, error, errorInfo: info });
+
+    // Report errors here
+    const { onError, FallbackComponent, ..._props } = this.props;
+    if (typeof onError === "function") {
+      try {
+        onError.call(this, error, info, _props);
+      } catch (e) {}
     }
+  }
 
-    closeErrorModal () {
-        this.setState({ hasError: false });
+  render() {
+    const { onError, FallbackComponent, children, ..._props } = this.props;
+    // if state contains error and in development environment we render fallback component
+    if (this.state.hasError) {
+      const { error, errorInfo } = this.state;
+      return (
+        <FallbackComponent
+          {..._props}
+          closeErrorModal={this.closeErrorModal}
+          error={error}
+          errorInfo={errorInfo}
+        />
+      );
     }
-
-    componentDidCatch (error, info) {
-        // Update state if error happens
-        this.setState({ hasError: true, error, errorInfo: info });
-
-        // Report errors here
-        const {onError, FallbackComponent, ..._props} = this.props;
-        if (typeof onError === 'function') {
-            try {
-                onError.call(this, error, info, _props);
-            } catch (e) {}
-        }
-    }
-
-    render () {
-        const {onError, FallbackComponent, children, ..._props} = this.props;
-        // if state contains error and in development environment we render fallback component
-        if (this.state.hasError) {
-            const { error, errorInfo } = this.state
-            return (
-                <FallbackComponent
-                    {..._props}
-                    closeErrorModal={this.closeErrorModal}
-                    error={error}
-                    errorInfo={errorInfo}
-                />
-            )
-        }
-        return children
-    }
+    return children;
+  }
 }
 ErrorBoundary.defaultProps = {
-    FallbackComponent: FallbackView
-}
+  FallbackComponent: FallbackView
+};
 
-function getDisplayName(WrappedComponent) {
-    return WrappedComponent.displayName || WrappedComponent.name || 'Component';
-}
-
-function curry(fn) {
-    if (typeof fn !== 'function') {
-        throw Error('curry only receive function params!')
-    }
-    let _len = fn.length, _args = [];
-
-    function _curry() {
-        var args = [].concat(_args);
-        if (arguments.length >= _len) {
-            _args = [];
-        } else if (arguments.length + _args.length > _len) {
-            _args = [];
-        }
-        _args = _args.concat([].slice.call(arguments));
-        if (_args.length === _len) {
-            var rst = fn.apply(null, _args);
-            _args = args;
-            return rst;
-        }
-        return _curry;
-    }
-    _curry.toString = function() {
-        return fn.toString();
-    }
-    return _curry;
-}
-
-
-const withErrorHandler = curry((FallbackComponent, Component) => {
-    const WithErrorHandler = props => {
-        const {onError} = props;
-        return (<ErrorBoundary FallbackComponent={FallbackComponent} onError={onError}>
-          <Component {...props} />
-        </ErrorBoundary>)
-    };
-    WithErrorHandler.displayName = `WithErrorHandler(${getDisplayName(Component)})`;
-    return WithErrorHandler;
-});
-
-export { 
-    withErrorHandler,
-    ErrorBoundary,
-    FallbackView
-}
-export default ErrorBoundary
+export { ErrorBoundary, FallbackView };
+export default ErrorBoundary;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,78 @@
-import React from 'react'
-import { withErrorHandler, ErrorBoundary, FallbackView } from './WithErrorHandler'
+import React from "react";
 
-const errorHandlerDecorator = withErrorHandler(FallbackView);
+let __ErrorBoundary;
+if (
+  process.env.NODE_ENV === "development" ||
+  process.env.ERROR_ENV === "development"
+) {
+  const { ErrorBoundary, FallbackView } = require("./WithErrorHandler");
 
-export { ErrorBoundary, withErrorHandler, errorHandlerDecorator, FallbackView }
-export default ErrorBoundary
+  const withErrorHandler = curry((FallbackComponent, Component) => {
+    const WithErrorHandler = props => {
+      const { onError } = props;
+      return (
+        <ErrorBoundary FallbackComponent={FallbackComponent} onError={onError}>
+          <Component {...props} />
+        </ErrorBoundary>
+      );
+    };
+    WithErrorHandler.displayName = `WithErrorHandler(${Component.displayName ||
+      Component.name ||
+      "Component"})`;
+    return WithErrorHandler;
+  });
+  __ErrorBoundary = ErrorBoundary;
+  exports.ErrorBoundary = ErrorBoundary;
+  exports.FallbackView = FallbackView;
+  exports.withErrorHandler = withErrorHandler;
+  exports.errorHandlerDecorator = withErrorHandler(FallbackView);
+} else {
+  // production or other env (not development)
+  // NOOP HOC
+  const withErrorHandler = curry((FallbackComponent, Component) => {
+    const WithErrorHandler = props => {
+      return <Component {...props} />;
+    };
+    return WithErrorHandler;
+  });
+  // NOOP ErrorBoundary
+  class ErrorBoundary extends React.Component {
+    render() {
+      return this.props.children;
+    }
+  }
+  __ErrorBoundary = ErrorBoundary;
+  exports.ErrorBoundary = ErrorBoundary;
+  exports.withErrorHandler = withErrorHandler;
+  exports.errorHandlerDecorator = withErrorHandler(void 0);
+}
+
+function curry(fn) {
+  if (typeof fn !== "function") {
+    throw Error("curry only receive function params!");
+  }
+  let _len = fn.length,
+    _args = [];
+
+  function _curry() {
+    var args = [].concat(_args);
+    if (arguments.length >= _len) {
+      _args = [];
+    } else if (arguments.length + _args.length > _len) {
+      _args = [];
+    }
+    _args = _args.concat([].slice.call(arguments));
+    if (_args.length === _len) {
+      var rst = fn.apply(null, _args);
+      _args = args;
+      return rst;
+    }
+    return _curry;
+  }
+  _curry.toString = function() {
+    return fn.toString();
+  };
+  return _curry;
+}
+
+export default __ErrorBoundary;
